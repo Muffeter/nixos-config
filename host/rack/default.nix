6c099bfb59cc2c05fbc5ff6faafb34ce9a96d5b7
sonmu <sonmu.def@gmail.com>
2025-01-15 04:14:58 +0000

    vaultwarden docker add

--- host/rack/default.nix
+++ host/rack/default.nix
@@ -1,19 +1,20 @@
 {config, pkgs, ... }: {
   imports = [
     ./hardware-configuration.nix
-    
-    
+    ../../docker/vaultwarden
   ];
 
   boot.tmp.cleanOnBoot = true;
   zramSwap.enable = false;
-  networking.hostName = "racknerd-e6845e";
+  networking.hostName = "rack";
   networking.domain = "";
+  networking.firewall.enable = false;
   users.users.root.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICx/NKon/8VBF3Bld4NvUIBdhgqxE8/VFWPJaPUvNR5u ming@LDS'' ];
   i18n.defaultLocale = "en_US.UTF-8";
 
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  virtualisation.docker.enable = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -28,7 +29,7 @@
   users.users.ming = {
     isNormalUser = true;
     description = "ming";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
     shell = pkgs.fish;
   };
 
