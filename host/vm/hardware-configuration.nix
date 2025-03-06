361f0930f4ee30abf1916fc4600a2ea521a6bee8
sonmu <sonmu.def@gmail.com>
2025-03-06 10:49:47 +0000

    vm hardware

--- host/vm/hardware-configuration.nix
+++ host/vm/hardware-configuration.nix
@@ -13,34 +13,33 @@
 
   fileSystems."/" =
     {
-      device = "/dev/disk/by-uuid/35b318e1-af58-4d0a-9dcf-e9d782bd38c5";
+      device = "/dev/sda2";
       fsType = "btrfs";
       options = [ "subvol=root" "compress=zstd" ];
     };
 
   fileSystems."/home" =
     {
-      device = "/dev/disk/by-uuid/35b318e1-af58-4d0a-9dcf-e9d782bd38c5";
+      device = "/dev/sda2";
       fsType = "btrfs";
-      options = [ "subvol=root" "compress=zstd" ];
+      options = [ "subvol=home" "compress=zstd" ];
     };
 
   fileSystems."/nix" =
     {
-      device = "/dev/disk/by-uuid/35b318e1-af58-4d0a-9dcf-e9d782bd38c5";
+      device = "/dev/sda2";
       fsType = "btrfs";
       options = [ "subvol=nix" "compress=zstd" "noatime" ];
     };
 
   fileSystems."/boot" =
     {
-      device = "/dev/disk/by-uuid/7B74-3C73";
+      device = "/dev/sda1";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/db959e38-70ad-4a33-85d0-d2db740e31f8"; }];
+    [{ device = "/dev/sda3"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
