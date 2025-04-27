a96e1efd62e916d220b25e0a5b7e0ac8320d09e0
Muffeter <sonmu.def@gmail.com>
2025-04-27 14:38:05 +0000

    mosdns

--- host/rain/default.nix
+++ host/rain/default.nix
@@ -2,10 +2,11 @@
   imports = [
     ./hardware-configuration.nix
     ../../docker/teamspeak
-    ../../docker/momWechat
-    ../../docker/langbot
-    ../../docker/filebox
-    ../../docker/aria2pro
+    ../../docker/mosdns
+    # ../../docker/momWechat
+    # ../../docker/langbot
+    # ../../docker/filebox
+    # ../../docker/aria2pro
   ];
 
   boot.tmp.cleanOnBoot = true;
@@ -25,14 +26,32 @@
 };
 
 
-#nix.settings.substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" ];
 nix.settings.experimental-features = [ "nix-command" "flakes" ];
+nix.settings.substituters = [ "https://mirrors.cernet.edu.cn/nix-channels/store" ];
   environment.systemPackages = with pkgs; [
     git
     neovim
     wget
     just
     fish
+    (let base = pkgs.appimageTools.defaultFhsEnvArgs; in
+      pkgs.buildFHSUserEnv (base // {
+      name = "fhs";
+      targetPkgs = pkgs: 
+        # pkgs.buildFHSUserEnv 只提供一个最小的 FHS 环境，缺少很多常用软件所必须的基础包
+        # 所以直接使用它很可能会报错
+        #
+        # pkgs.appimageTools 提供了大多数程序常用的基础包，所以我们可以直接用它来补充
+        (base.targetPkgs pkgs) ++ (with pkgs; [
+          pkg-config
+          ncurses
+          # 如果你的 FHS 程序还有其他依赖，把它们添加在这里
+        ]
+      );
+      profile = "export FHS=1";
+      runScript = "bash";
+      extraOutputsToInstall = ["dev"];
+    }))
   ];
   users.users.ming = {
     isNormalUser = true;
